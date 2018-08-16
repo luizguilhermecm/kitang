087822a4aacc351b3eb632c14832a2119b759436
luizmartins <luizmartins@trt15.jus.br>
2018-08-16 17:00:05 -0300

    tags ok

--- src/app/todos/tag/tag.service.ts
+++ src/app/todos/tag/tag.service.ts
@@ -5,8 +5,8 @@ import { Tag } from "./tag";
 
 // const API = 'http://localhost:3000';
 // const API = 'http://localhost:4567/kit_api/tags';
-// const API = 'http://lgcm.com.br/kit_api/tags';
-const API = 'http://168.235.81.92/kit_api/tags';
+const API = 'https://lgcm.com.br/kit_api/tags';
+//const API = 'http://168.235.81.92/kit_api/tags';
 
 @Injectable({ providedIn: 'root' })
 export class TagService {
